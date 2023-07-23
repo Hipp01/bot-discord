2046e85ff75128ef2275e50d21568ab7c3d1be09
ProjectMagic18 <guillaumelereunionnais@gmail.com>
2023-07-23 16:29:02 +0200

    Import the funny command

--- chat.py
+++ chat.py
@@ -2,6 +2,7 @@ import discord, random, json
 from discord.ext import commands
 from morpion import Morpion
 from pendu import Pendu
+from funny import Funny
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -15,6 +16,7 @@ async def on_ready():
     print("Bot is ready !")
     await bot.add_cog(Morpion(bot))
     await bot.add_cog(Pendu(bot))
+    await bot.add_cog(Funny(bot))
 
 @bot.command()
 async def ping(ctx):
